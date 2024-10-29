ddb2fc0ff0d3b50285caa23fe16167cc4b1b3551
Gabriella Pouso <pouso.gabi@gmail.com>
2024-10-29 15:45:46 -0300

    desafio

--- main.cpp
+++ main.cpp
@@ -2,21 +2,18 @@
 
 using namespace std;
 
-// Função para solicitar um número ao usuário
-int solicitarNumero() {
+int obterNumero() {
     int numero;
     cout << "Por favor, insira um número inteiro: ";
     cin >> numero;
     return numero;
 }
 
-// Função para calcular o antecessor e o sucessor
 void calcularAntecessorSucessor(int numero, int &antecessor, int &sucessor) {
     antecessor = numero - 1;
     sucessor = numero + 1;
 }
 
-// Função para mostrar a tabuada de multiplicação
 void mostrarTabuada(int numero) {
     cout << "\nTabuada de " << numero << ":\n";
     for (int i = 1; i <= 10; i++) {
@@ -24,7 +21,6 @@ void mostrarTabuada(int numero) {
     }
 }
 
-// Função para mostrar os próximos três números com contagem de dois em dois
 void mostrarProximosNumeros(int numero) {
     cout << "\nPróximos três números com contagem de dois em dois:\n";
     for (int i = 1; i <= 3; i++) {
@@ -32,11 +28,10 @@ void mostrarProximosNumeros(int numero) {
     }
 }
 
-// Função principal
 int main() {
     int numero, antecessor, sucessor;
 
-    numero = solicitarNumero();
+    numero = obterNumero();
     calcularAntecessorSucessor(numero, antecessor, sucessor);
     
     cout << "Antecessor: " << antecessor << ", Sucessor: " << sucessor << endl;
