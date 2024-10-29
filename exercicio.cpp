2d9ae4be88348b9bdbdd8cee468df99c1e506046
Gabriella Pouso <pouso.gabi@gmail.com>
2024-10-29 17:07:58 -0300

    ternário

--- exercicio.cpp
+++ exercicio.cpp
@@ -5,12 +5,7 @@ int main(){
     scanf("%d",&numero1);
     printf("Digite outro número: ");
     scanf("%d",&numero2);
-    if(numero1>numero2){
-        maior=numero1;
-    }
-    else{
-        maior=numero2;
-    }
+    maior=numero1>numero2?numero1:numero2;
     printf("Maior: %d\n",maior);
     return 0;
 }
