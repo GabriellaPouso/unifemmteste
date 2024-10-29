c60e017b53edfc6d8c163a0a6c2d31234f1407ed
Gabriella Pouso <pouso.gabi@gmail.com>
2024-10-29 16:34:09 -0300

    ativ e des

--- exercicio.cpp
+++ exercicio.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
+#include <stdio.h>
 int main(){
-    float n=3.2;
-    float i=2.5;
-    float resultado=10;
-    resultado=resultado+i+n*3-2;
-    printf("%.2lf\n", resultado);
+    int x=18;
+    int resposta = x-2;
+
+    printf("O valor da variável resposta é: %d\n", resposta);
     return 0;
 }
