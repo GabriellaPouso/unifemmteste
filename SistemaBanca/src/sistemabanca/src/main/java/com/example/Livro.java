0d2c78a3be6eba8ea49e069ae3062dd564dcb5c6
Gabriella Pouso <pouso.gabi@gmail.com>
2024-12-03 13:53:28 -0300

    maven

---
+++ SistemaBanca/src/sistemabanca/src/main/java/com/example/Livro.java
@@ -0,0 +1,16 @@
+public abstract class Livro implements Produto {
+    protected String titulo;
+    protected String autor;
+    protected double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Preço: " + preco);
+    }
+}
---
+++ SistemaBanca/src/sistemabanca/src/main/java/com/example/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+public class Main {
+    public static void main(String[] args) {
+        Produto livroFisico = new LivroFisico("Java para Iniciantes", "Autor A", 50.00, 10.00);
+        Produto ebook = new Ebook("Aprendendo Java", "Autor B", 30.00, 5.00);
+        Produto miniLivro = new MiniLivro("Java Básico", "Autor C", 20.00);
+        Produto revista = new Revista("Tecnologia Hoje", "Editora X", 15.00);
+
+        System.out.println("Livro Físico:");
+        ((LivroFisico) livroFisico).exibirDetalhes();
+        System.out.println("Preço com desconto + frete: " + livroFisico.calcularPrecoComDesconto());
+
+        System.out.println("\nEbook:");
+        ((Ebook) ebook).exibirDetalhes();
+        System.out.println("Preço com desconto + taxa de licenciamento: " + ebook.calcularPrecoComDesconto());
+
+        System.out.println("\nMiniLivro:");
+        ((MiniLivro) miniLivro).exibirDetalhes();
+        System.out.println("Preço com desconto: " + miniLivro.calcularPrecoComDesconto());
+
+        System.out.println("\nRevista:");
+        ((Revista) revista).exibirDetalhes();
+        System.out.println("Preço com desconto: " + revista.calcularPrecoComDesconto());
+    }
+}
